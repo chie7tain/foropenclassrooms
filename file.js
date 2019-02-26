6f9d2ecd52aea5cc6eed545297a0a6f188340d97
Chie7tain <fredrickokwuobi@gmail.com>
2019-02-26 12:56:20 +0100

    Add some dazzle to js

--- file.js
+++ file.js
@@ -3,4 +3,8 @@ var greetings = 'Hello Openclassrooms';
 btn.addEventListener('click',myFunction);
  function myFunction (){
  	alert(greetings);
+ 	var newFriend = prompt('Whats your beautifull name?!');
+ 	alert(newFriend + ' it is absolutely fantastic to meet you'!!);
+ 	var care = prompt(newFriend  + ' are You from outerspace? you look familiar!!!');
+ 	alert('INCREDIIBLEE no wonder... how is krypton?!!');
  }
